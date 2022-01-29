a0b87034b508b05cb56e2a708a7aa270d41f8ecf
fengxiakira <limengyuanwinnie@163.com>
2022-01-29 17:33:50 -0500

    done: rename files, observe todo: fix eventhandler path

--- downloadCleaner/EventHandler.py
+++ downloadCleaner/EventHandler.py
@@ -17,38 +17,56 @@ import stat
 
 class DownloadsCleanEventHandler(FileSystemEventHandler):
     def __init__(self, path: Path):
-        # all are PosixPath object
+        # BUG: self.files shall be dynamic, not be fixed at __init,
+        # That's why it cannot configured modification of dir
+        # TODO: fix this in on_modified
         self.files = [f for f in path.iterdir() if f.is_file()]
         self.path = path
-        # print(self.files)
+
+    def rename_file(self,file:Path,dist:Path):
+        '''
+        helper function: If a file of the same
+        name already exists in the destination folder, the file name is numbered and
+        incremented until the filename is unique (prevents overwriting files).    
         
-    
-    # TODO:ensure depulicated files not be overwritten
-    def rename_file():
-        # return new path here
-        pass
+        :param Path source: source of file to be moved
+        :param Path destination_path: path to destination directory  
+        '''
+        if Path( dist/ file.name).exists():
+            copy = 0
+            while True:
+                copy += 1
+                new_path = dist / f'{file.stem}-{copy}{file.suffix}'
+                if not new_path.exists():
+                    return new_path
+        else:
+            return dist / file.name
+                
 
-
-    # TODO:add event as argv
-    def on_modified(self):
+        
+    def on_modified(self, event):
+        print("call on_modified")
         for f in self.files:
+            # .DS_Store doesn't count as suffix
+            if f.name == '.DS_Store':
+                continue        
             suffix = f.suffix.lower()
             # check if suffix in extension_paths dict. if yes, then move file to that dir
             if suffix in extension_paths:
+                # dist here is folder
                 dist = self.path / extension_paths[suffix]
+                # check if folder exists
                 if not dist.exists():
-                    os.makedirs(dist,mode=711) 
-                    os.chmod(dist,stat.S_IRWXU)            
+                    os.makedirs(dist, mode=711)
+                    os.chmod(dist, stat.S_IRWXU)
+                dist = self.rename_file(f,dist)
                 shutil.move(src=f, dst=dist)
+                print("Moved f to",dist)
             else:
                 # move to other dir
                 dist = self.path / "other"
                 if not dist.exists():
-                    os.makedirs(dist,mode=711)
+                    os.makedirs(dist, mode=711)
                 shutil.move(src=f, dst=dist)
 
-
-######################TEST#######################################
-test = DownloadsCleanEventHandler(Path.home() / "Downloads")
-# test.on_modified()
 
--- downloadCleaner/app.py
+++ downloadCleaner/app.py
@@ -1,36 +1,31 @@
-'''
-1. automatically observe the download folder, Track Download folder
-2. Clear current files in the folder, 归类Download现有文件夹内的文件
-  - 去新的文件夹
-3. move different files going to different folder , 
-based on its extention name
-  - use a dict to organize it?
+"""
+
 4. Run in the background continueslly
 5. Run when PC is on
-'''
+"""
 
 import time
 from watchdog.observers import Observer
 from pathlib import Path
 from extension import extension_paths
 from EventHandler import DownloadsCleanEventHandler
- 
+
 
 # if __name__ == '__main__' this app.py run as the main programe, not imported by others
-if __name__ == '__main__':
-  # / only for path object
-  watch_path = Path.home()/'Downloads'
-  observer = Observer()
-  # TODO:event_handler : FileSystemEventhandler,
-  # recursive=True watch the whole directory
-  event_handler = DownloadsCleanEventHandler(watch_path)
-  observer.schedule(event_handler, str(watch_path), recursive=True)
-  observer.start()
-  try:
-      while True:
-          time.sleep(120)
-  except KeyboardInterrupt:
-      observer.stop()
-  # This blocks the calling thread until the thread whose join() method is called terminates – either normally or through an unhandled exception or until the optional timeout occurs.
-  observer.join()
-
+if __name__ == "__main__":
+    # / only for path object
+    watch_path = Path.home() / "Downloads"
+    # TODO:event_handler : FileSystemEventhandler,
+    event_handler = DownloadsCleanEventHandler(watch_path)
+    # recursive=True watch the whole directory  
+    observer = Observer()
+    observer.schedule(event_handler, str(watch_path), recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(2)
+            print('observe')
+    except KeyboardInterrupt:
+        observer.stop()
+    # This blocks the calling thread until the thread whose join() method is called terminates – either normally or through an unhandled exception or until the optional timeout occurs.
+    observer.join()
